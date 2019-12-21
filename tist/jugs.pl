ea108349154a00ae3ff18554aea31615b3dc61cc
Markus Triska <triska@metalevel.at>
2019-12-21 20:38:01 +0100

    better variable names, following a suggestion by C. M. Sperberg-McQueen

--- tist/jugs.pl
+++ tist/jugs.pl
@@ -5,16 +5,18 @@ jug_capacity(b, 5).
 jug_capacity(c, 3).
 
 moves(Jugs) -->
-        { memberchk(jug(a,4), Jugs),
-          memberchk(jug(b,4), Jugs) }.
-moves(Jugs0) -->
-        { select(jug(AID,AF), Jugs0, Jugs1), AF #> 0,
-          select(jug(BID,BF), Jugs1, Jugs2), jug_capacity(BID, BC), BF #< BC,
-          Move #= min(AF,BC-BF),
-          AF1 #= AF - Move,
-          BF1 #= BF + Move },
-        [from_to(AID,BID)],
-        moves([jug(AID,AF1),jug(BID,BF1)|Jugs2]).
+        { member(jug(a,4), Jugs),
+          member(jug(b,4), Jugs) }.
+moves(Jugs0) --> [from_to(From,To)],
+        { select(jug(From,FromFill0), Jugs0, Jugs1),
+          FromFill0 #> 0,
+          select(jug(To,ToFill0), Jugs1, Jugs),
+          jug_capacity(To, ToCapacity),
+          ToFill0 #< ToCapacity,
+          Move #= min(FromFill0, ToCapacity-ToFill0),
+          FromFill #= FromFill0 - Move,
+          ToFill #= ToFill0 + Move },
+        moves([jug(From,FromFill),jug(To,ToFill)|Jugs]).
 
 %?- length(Ms, _), phrase(moves([jug(a,8),jug(b,0),jug(c,0)]), Ms).
 %@ Ms = [from_to(a,b),from_to(b,c),from_to(c,a),from_to(b,c),from_to(a,b),from_to(b,c),from_to(c,a)] .
