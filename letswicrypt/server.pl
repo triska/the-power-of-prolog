5c4ce227b75c6804d1f012bd1da3b56446a30dab
Markus Triska <triska@metalevel.at>
2020-06-10 20:10:42 +0200

    remove Proloxy and LetSWICrypt from the distribution

--- letswicrypt/server.pl
+++
@@ -1,9 +0,0 @@
-:- use_module(library(http/http_unix_daemon)).
-:- use_module(library(http/thread_httpd)).
-:- use_module(library(http/http_dispatch)).
-
-:- http_handler(/, handle_request, [prefix]).
-
-handle_request(_Request) :-
-        format("Content-type: text/plain~n~n"),
-        format("Hello!").
