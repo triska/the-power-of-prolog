a37a7267b7616a73253c22af1537c40069259c9f
Markus Triska <triska@metalevel.at>
2021-02-28 09:58:07 +0100

    use the new path specification of the Scryer Prolog HTTP server

--- prolog/server.pl
+++ prolog/server.pl
@@ -1,7 +1,7 @@
 :- use_module(library(http/http_server)).
 
 run(Port) :-
-        http_listen(Port, [get('', request_response)]).
+        http_listen(Port, [get(/, request_response)]).
 
 request_response(_, Response) :-
         http_status_code(Response, 200),
@@ -10,5 +10,7 @@ request_response(_, Response) :-
 
 /* - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 ?- run(6080).
-Listening at port 6080
+%@ Listening at port 6080
+%@ 2021-02-22 (19:44:11) get /
+%@ 2021-02-22 (19:44:11) get /favicon.ico
 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - */
